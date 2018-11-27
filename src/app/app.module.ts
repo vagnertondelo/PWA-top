4647ef37b690595f9e4157d42cddd75a08c867af
Vagner <vagnertondellcomo@gmail.com>
2018-11-27 10:26:46 -0200

    page inicial, header e footer

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { InicialComponent } from './inicial/inicial.component';
+
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicialComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import {HeaderComponent} from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+    declarations: [HeaderComponent, FooterComponent],
+    exports: [HeaderComponent, FooterComponent]
+})
+
+
+export class CoreModule {}
